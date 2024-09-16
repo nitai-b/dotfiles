ecea6ff8dd3c24e8effb97bd8bbd3ba4834cafe2
nitai-b <nitaibarran5@gmail.com>
2024-09-15 20:55:13 -0400

    changes to tmux sessionizer

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  selected=$(find ~/projects/work ~/projects/personal/ ~/.myTools/ ~/.myScripts/ ~/ -mindepth 1 -maxdepth 2 -type d | fzf)
+  selected=$(find ~/projects/work ~/projects/personal/ ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
