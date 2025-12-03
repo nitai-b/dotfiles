29d4ceedfaca8326d300f328c8c0365eebeec683
nitai-b <nitaibarran5@gmail.com>
2025-12-03 13:46:03 -0400

    removing build-incon.sh

--- build-incon.sh
+++
@@ -1,7 +0,0 @@
-cd /home/nitai/projects/work/incon24/
-git reset --hard
-cp app.json.nbarran app.json
-npx expo prebuild --clean
-eas build --platform android --profile preview --local
-cp app.json.kbelgrove app.json
-nautilus .
