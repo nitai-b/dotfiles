564c0b76ea5cdda6c79f42d8d5ff6532692a9bdc
nitai-b <nitaibarran5@gmail.com>
2025-12-28 10:15:54 -0400

    correct commands in bash script for screen timeout toggle

--- toggle-screen-timeout.sh
+++ toggle-screen-timeout.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Check current screen timeout (in seconds)
-TIMEOUT=$(gsettings get org.gnome.desktop.session idle-delay)
+CURRENT=$(dconf read /org/gnome/desktop/session/idle-delay)
 
-if [ "$TIMEOUT" == "uint32 0" ]; then
-  # Currently set to never turn off, so enable timeout (5 minutes = 300 seconds)
-  gsettings set org.gnome.desktop.session idle-delay 480
+if [ "$CURRENT" == "uint32 0" ]; then
+  dconf write /org/gnome/desktop/session/idle-delay "uint32 300"
   notify-send "Screen timeout enabled (5 minutes)"
 else
-  # Currently has timeout, so disable it
-  gsettings set org.gnome.desktop.session idle-delay 0
+  dconf write /org/gnome/desktop/session/idle-delay "uint32 0"
   notify-send "Screen timeout disabled"
 fi
